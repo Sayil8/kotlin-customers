07cd69cddf9e6293bde1623ac820b276b4d0d311
Sayil8 <sayil_poncho@hotmail.com>
2020-08-22 15:57:32 -0500

    First routes

--- src/main/kotlin/com/microservices/chapter11/CustomerController.kt
+++ src/main/kotlin/com/microservices/chapter11/CustomerController.kt
@@ -1,4 +1,23 @@
 package com.microservices.chapter11
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ConcurrentHashMap
+
+@RestController
 class CustomerController {
+
+    companion object {
+        val initialCustomers = arrayOf(Customer(1, "Kotlin"),
+                Customer(2, "Spring"),
+                Customer(3, "Microservice"))
+        val customers = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
+    }
+
+    @GetMapping("/customers")
+    fun getAllCustomer() = customers.values.toList()
+
+    @GetMapping("/customer/{id}")
+    fun getCustomer(@PathVariable id: Int) = customers[id]
 }
